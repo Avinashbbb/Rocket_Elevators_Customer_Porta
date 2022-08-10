49494945a7743c995242564fa535929587f6b011
Avinashbbb <avinash.bbb@gmail.com>
2022-08-10 09:54:07 -0400

    going to scatfold the identy

---
+++ Data/Avinash_PortalContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace New_folder.Data;
+
+public class Avinash_PortalContext : IdentityDbContext
+{
+    public Avinash_PortalContext(DbContextOptions<Avinash_PortalContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+console.log("hello");
+selectCustomer()
+
+
+
+
+function selectCustomer(){
+    fetch("https://apicsharpavinash.herokuapp.com/api/Customer")
+        .then(response=>{
+            return response.json();
+        }).then(json=>{
+            console.log(json)
+        })
+}
